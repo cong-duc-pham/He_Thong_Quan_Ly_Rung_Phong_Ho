95f4bd0ba870c182a73ce71092b5498d30959a6a
cong-duc-pham <congducpham2010@gmail.com>
2026-02-04 15:08:50 +0700

    Them phan ca lam viec

--- QuanLyRungPhongHo/Models/CaLamViec.cs
+++ QuanLyRungPhongHo/Models/CaLamViec.cs
@@ -1,6 +1,4 @@
-﻿
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,27 +8,20 @@ namespace QLRungPhongHo.Models
     public class CaLamViec
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int MaCa { get; set; }
 
         [Required]
-        [MaxLength(100)]
-        public string TenCa { get; set; } = string.Empty;
+        [StringLength(100)]
+        public string TenCa { get; set; }
 
         [Required]
-        [Column(TypeName = "time(7)")]
         public TimeSpan GioBatDau { get; set; }
 
         [Required]
-        [Column(TypeName = "time(7)")]
         public TimeSpan GioKetThuc { get; set; }
 
-        public string? MoTa { get; set; }
+        public string MoTa { get; set; }
 
-        [Required]
         public bool TrangThai { get; set; } = true;
-
-        // Navigation property
-        public virtual ICollection<LichLamViec> LichLamViecs { get; set; } = new List<LichLamViec>();
     }
 }
